e00f275b27e8dd7e88d95199a5bfbda4afaa2390
Deadpico <beland14@student.westerdals.no>
2015-05-13 19:19:32 +0200

    .

---
+++ Assets/Scripts/BrickBoxDestroy.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrickBoxDestroy : MonoBehaviour {
+
+    public GameObject block;
+
+    private Player playerClass;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerClass = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.gameObject.tag == "Player")
+            if(playerClass.marioHealth == 2)
+             {
+                Destroy(block);
+            }
+
+    }
+}
